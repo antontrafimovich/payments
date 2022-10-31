15e868082f02e3d0e962335c5aee0cd03c2edfc5
Anton Trafimovich <antont@compatibl.com>
2022-10-31 17:58:55 +0100

    Added jss. Styled header with jss.

--- apps/web-client/src/ui-kit/table/header/table-header.tsx
+++ apps/web-client/src/ui-kit/table/header/table-header.tsx
@@ -1,19 +1,33 @@
+import { createUseStyles } from "react-jss";
 import { Column } from "../table.model";
 
 export interface TableHeaderProps {
   columns: Column[];
 }
 
+const useStyles = createUseStyles({
+  header: {
+    display: "flex",
+    "& > div + div": {
+      marginLeft: 1,
+    },
+  },
+  column: {
+    width: 200,
+    background: "#98c1d9",
+    padding: "8px 8px",
+  },
+});
+
 export const TableHeader = (props: TableHeaderProps) => {
   const { columns } = props;
 
+  const classes = useStyles();
+
   return (
-    <div style={{ display: "flex" }}>
+    <div className={classes.header}>
       {columns.map((column) => (
-        <div
-          style={{ width: "200px", background: "#98c1d9", padding: "8px 8px" }}
-          key={column.id}
-        >
+        <div className={classes.column} key={column.id}>
           {column.title}
         </div>
       ))}
